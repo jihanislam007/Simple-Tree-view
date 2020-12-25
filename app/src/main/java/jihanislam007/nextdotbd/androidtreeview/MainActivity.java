9d0eaa9c35c7440ce015015146fe64555b5a110d
jihan <jihanislam007>
2020-12-25 08:53:21 +0600

    final commit v1

---
+++ app/src/main/java/jihanislam007/nextdotbd/androidtreeview/MainActivity.java
@@ -0,0 +1,51 @@
+/****https://www.youtube.com/watch?v=y7qbug00VGs****/
+/****https://github.com/mayankneeds/TreeView-Android****/
+
+
+package jihanislam007.nextdotbd.androidtreeview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import jihanislam007.nextdotbd.androidtreeview.atv.model.TreeNode;
+import jihanislam007.nextdotbd.androidtreeview.atv.view.AndroidTreeView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //Root
+        TreeNode root = TreeNode.root();
+
+        //Parent
+        MyHolder.IconTreeItem nodeItem = new MyHolder.IconTreeItem(R.drawable.ic_arrow_drop_down, "Parent");
+        TreeNode parent = new TreeNode(nodeItem).setViewHolder(new MyHolder(getApplicationContext(), true, MyHolder.DEFAULT, MyHolder.DEFAULT));
+
+        //Child
+        MyHolder.IconTreeItem childItem = new MyHolder.IconTreeItem(R.drawable.ic_folder, "Child");
+        TreeNode child = new TreeNode(childItem).setViewHolder(new MyHolder(getApplicationContext(), false, R.layout.child, 25));
+
+        //Sub Child
+        MyHolder.IconTreeItem subChildItem = new MyHolder.IconTreeItem(R.drawable.ic_folder, "Sub Child");
+        TreeNode subChild = new TreeNode(subChildItem).setViewHolder(new MyHolder(getApplicationContext(), false, R.layout.child, 50));
+
+        //Add sub child.
+        child.addChild(subChild);
+
+
+        //Add child.
+        parent.addChildren(child);
+        root.addChild(parent);
+
+        //Add AndroidTreeView into view.
+        AndroidTreeView tView = new AndroidTreeView(getApplicationContext(), root);
+        ((LinearLayout) findViewById(R.id.ll_parent)).addView(tView.getView());
+
+    }
+}
